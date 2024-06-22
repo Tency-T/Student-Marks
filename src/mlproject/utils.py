f3910daf281cd19cdecd99d02767dca111ea6e8d
Tency-T <tencytrvd@gmail.com>
2024-06-22 21:58:37 +0530

    Data Ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established")
+
+        df = pd.read_sql_query('SELECT * FROM students', mydb)
+        mydb.close()
+
+        if df.empty:
+            logging.warning("The query returned an empty DataFrame.")
+        
+        logging.info(f"DataFrame shape: {df.shape}")
+        logging.info(f"DataFrame head: \n{df.head()}")
+
+        return df
+
+    except Exception as ex:
+        raise CustomException(ex)
