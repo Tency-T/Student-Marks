38e3978323f0325cba9e3f559519087d95106c30
Tency-T <tencytrvd@gmail.com>
2024-06-30 19:29:49 +0530

    Model training and dagshub

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-End to End Data Science Project
+##End to End Data Science Project
+
+import os
+os.environ['MLFLOW_TRACKING_URI'] = 'https://dagshub.com/tencytrvd/Student-Marks.mlflow'
+
+import dagshub
+dagshub.init(repo_owner='tencytrvd', repo_name='Student-Marks', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -1,12 +1,20 @@
 import os
 import sys
-from dataclasses import dataclass
 
+from dataclasses import dataclass
+from urllib.parse import urlparse
+
+import mlflow
+import dagshub
+import mlflow.sklearn
+
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
-    RandomForestRegressor,
-    GradientBoostingRegressor
+    GradientBoostingRegressor,
+    RandomForestRegressor
 )
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -16,8 +24,10 @@ from xgboost import XGBRegressor
 
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+from src.mlproject.utils import save_object,evaluate_models
 
-from src.mlproject.utils import save_object, evaluate_models
+# Initialize DagsHub
+dagshub.init(repo_owner='tencytrvd', repo_name='Student-Marks', mlflow=True)
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,6 +36,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -92,6 +108,49 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
+            print("this is the best model: " + best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model = ""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            # Set MLflow tracking URI to DagsHub
+            mlflow.set_tracking_uri("https://dagshub.com/tencytrvd/Student-Marks.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            #MLflow PIPELINE
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
